3b51d3f6a34cbefc269f0819ef59c189d1514b1d
LunarTides <31688109+LunarTides@users.noreply.github.com>
2025-03-13 13:37:46 +0100

    Use bitmask instead of color and type for cards

--- build.zig
+++ build.zig
@@ -24,12 +24,13 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
+        // .strip = true,
     });
 
     // This creates another `std.Build.Step.Compile`, but this one builds an executable
     // rather than a static library.
     const exe = b.addExecutable(.{
-        .name = "samples",
+        .name = "zuno",
         .root_module = exe_mod,
     });
 
--- src/main.zig
+++ src/main.zig
@@ -1,36 +1,37 @@
 const std = @import("std");
 
-const Color = enum {
-    none,
-    red,
-    green,
-    blue,
-    yellow,
+const Color = enum(u7) {
+    none = 0b0000000,
+    red = 0b0010000,
+    green = 0b0100000,
+    blue = 0b0110000,
+    yellow = 0b1000000,
 };
 
-const Type = enum {
-    one,
-    two,
-    three,
-    four,
-    five,
-    six,
-    seven,
-    eight,
-    nine,
-    skip,
-    reverse,
-    plus_two,
-    wildcard,
-    plus_four,
+const Symbol = enum(u7) {
+    // Start at one since that makes sense.
+    one = 0b0000001,
+    two = 0b0000010,
+    three = 0b0000011,
+    four = 0b0000100,
+    five = 0b0000101,
+    six = 0b0000110,
+    seven = 0b0000111,
+    eight = 0b0001000,
+    nine = 0b0001001,
+    skip = 0b0001010,
+    reverse = 0b0001011,
+    plus_two = 0b0001100,
+    wildcard = 0b0001101,
+    plus_four = 0b0001110,
 };
 
 const Player = struct {
     hand: std.ArrayList(Card),
 
-    fn init(gpa: std.mem.Allocator) Player {
+    fn init(gpa: *const std.mem.Allocator) Player {
         return Player{
-            .hand = std.ArrayList(Card).init(gpa),
+            .hand = std.ArrayList(Card).init(gpa.*),
         };
     }
 
@@ -38,51 +39,46 @@ const Player = struct {
         self.hand.deinit();
     }
 
-    fn draw_card(self: *Player) !void {
-        const card = Card.random();
+    fn draw_card(self: *Player, rng: *const std.Random) !void {
+        const card = Card.random(rng);
         try self.hand.append(card);
     }
 };
 
 const Card = struct {
-    color: Color,
-    type: Type,
+    bitmask: u7,
 
-    fn init(color: Color, @"type": Type) Card {
-        var _color = color;
-        const _type = @"type";
+    fn init(bitmask: u7) Card {
+        var color = get_color_from_bitmask(bitmask);
+        const symbol = get_symbol_from_bitmask(bitmask);
 
-        if (is_type_colorless(_type)) {
-            _color = Color.none;
+        if (is_symbol_colorless(symbol)) {
+            color = Color.none;
         } else if (color == Color.none) {
             // Default to red. This should be handled correctly in other functions.
-            _color = Color.red;
+            color = Color.red;
         }
 
         return Card{
-            .color = _color,
-            .type = _type,
+            .bitmask = create_bitmask(color, symbol),
         };
     }
 
-    fn random() Card {
-        const raw_seed: u128 = @bitCast(std.time.nanoTimestamp());
-        const seed: u64 = @truncate(raw_seed);
-        var random_number_generator = std.Random.DefaultPrng.init(seed);
-        const rng = random_number_generator.random();
+    fn random(rng: *const std.Random) Card {
+        var color: Color = rng.enumValue(Color);
+        const symbol: Symbol = rng.enumValue(Symbol);
 
-        var color: Color = @enumFromInt(rng.uintAtMost(u8, 4));
-        const @"type": Type = @enumFromInt(rng.uintAtMost(u8, 13));
-
-        if (!is_type_colorless(@"type") and color == Color.none) {
-            color = @enumFromInt(rng.uintAtMost(u8, 3) + 1);
+        if (!is_symbol_colorless(symbol)) {
+            while (color == Color.none) {
+                color = rng.enumValue(Color);
+            }
         }
 
-        return init(color, @"type");
+        return init(create_bitmask(color, symbol));
     }
 
-    fn name(self: Card, gpa: std.mem.Allocator) ![]u8 {
-        const color = switch (self.color) {
+    fn name(self: Card, gpa: *const std.mem.Allocator) ![]u8 {
+        const color = switch (get_color_from_bitmask(self.bitmask)) {
             Color.none => "",
             Color.red => "Red ",
             Color.green => "Green ",
@@ -90,28 +86,40 @@ const Card = struct {
             Color.yellow => "Yellow ",
         };
 
-        const @"type" = switch (self.type) {
-            Type.one => "1",
-            Type.two => "2",
-            Type.three => "3",
-            Type.four => "4",
-            Type.five => "5",
-            Type.six => "6",
-            Type.seven => "7",
-            Type.eight => "8",
-            Type.nine => "9",
-            Type.skip => "Skip",
-            Type.reverse => "Reverse",
-            Type.plus_two => "+2",
-            Type.wildcard => "Wildcard",
-            Type.plus_four => "+4",
+        const symbol = switch (get_symbol_from_bitmask(self.bitmask)) {
+            Symbol.one => "1",
+            Symbol.two => "2",
+            Symbol.three => "3",
+            Symbol.four => "4",
+            Symbol.five => "5",
+            Symbol.six => "6",
+            Symbol.seven => "7",
+            Symbol.eight => "8",
+            Symbol.nine => "9",
+            Symbol.skip => "Skip",
+            Symbol.reverse => "Reverse",
+            Symbol.plus_two => "+2",
+            Symbol.wildcard => "Wildcard",
+            Symbol.plus_four => "+4",
         };
 
-        return try std.fmt.allocPrint(gpa, "{s}{s}", .{ color, @"type" });
+        return try std.fmt.allocPrint(gpa.*, "{s}{s}", .{ color, symbol });
     }
 
-    fn is_type_colorless(@"type": Type) bool {
-        return @"type" == Type.wildcard or @"type" == Type.plus_four;
+    inline fn is_symbol_colorless(symbol: Symbol) bool {
+        return symbol == Symbol.wildcard or symbol == Symbol.plus_four;
+    }
+
+    inline fn get_color_from_bitmask(bitmask: u7) Color {
+        return @enumFromInt(bitmask & 0b1110000);
+    }
+
+    inline fn get_symbol_from_bitmask(bitmask: u7) Symbol {
+        return @enumFromInt(bitmask & 0b0001111);
+    }
+
+    inline fn create_bitmask(color: Color, symbol: Symbol) u7 {
+        return @intFromEnum(color) | @intFromEnum(symbol);
     }
 };
 
@@ -120,20 +128,25 @@ pub fn main() !void {
     // defer debug_allocator.deinit();
     const gpa = debug_allocator.allocator();
 
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello, {s}!\n", .{"world"});
+    const raw_seed: u128 = @bitCast(std.time.nanoTimestamp());
+    const seed: u64 = @truncate(raw_seed);
+    var random_number_generator = std.Random.DefaultPrng.init(seed);
+    const rng = random_number_generator.random();
 
-    var player1 = Player.init(gpa);
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Here are (3) randomly generated cards:\n", .{});
+
+    var player1 = Player.init(&gpa);
     defer player1.deinit();
 
     // var player2 = Player.init(gpa);
     // defer player2.deinit();
 
-    try player1.draw_card();
-    try player1.draw_card();
-    try player1.draw_card();
+    try player1.draw_card(&rng);
+    try player1.draw_card(&rng);
+    try player1.draw_card(&rng);
 
     for (player1.hand.items) |card| {
-        try stdout.print("{s}\n", .{try card.name(gpa)});
+        try stdout.print("{s}\n", .{try card.name(&gpa)});
     }
 }
